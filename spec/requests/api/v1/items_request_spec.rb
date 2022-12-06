94963ea6f4471c4936e7a5e8740a41c9f9d68389
Emily Port <emilylport@gmail.com>
2022-12-05 17:55:04 -0700

    all tests pass for item show api endpoint

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -28,4 +28,31 @@ describe "Items API endpoints" do
       expect(item[:attributes][:merchant_id]).to be_a(Integer)
     end
   end
+
+  it "can get one item by its id" do 
+    id = create(:item).id
+
+    get "/api/v1/items/#{id}"
+
+    item = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(response).to be_successful
+    # require 'pry'; binding.pry
+
+    expect(item).to have_key(:id)
+    expect(item[:id].to_i).to eq(id)
+  
+    expect(item[:attributes]).to have_key(:name)
+    expect(item[:attributes][:name]).to be_a(String)
+
+    expect(item[:attributes]).to have_key(:description)
+    expect(item[:attributes][:description]).to be_a(String)
+
+    expect(item[:attributes]).to have_key(:unit_price)
+    expect(item[:attributes][:unit_price]).to be_a(Float)
+
+    expect(item[:attributes]).to have_key(:merchant_id)
+    expect(item[:attributes][:merchant_id]).to be_a(Integer)
+
+  end
 end
