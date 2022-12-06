7ef240b59df70d43b7d66489b913ec1dcbf56669
Emily Port <emilylport@gmail.com>
2022-12-06 14:23:19 -0700

    can update one item but need to make edge case pass for bad merchant id

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -16,6 +16,10 @@ class Api::V1::ItemsController < ApplicationController
     render json: ItemSerializer.new(Item.create(item_params)), status: 201
   end
 
+  def update 
+    render json: ItemSerializer.new(Item.update(params[:id], item_params))
+  end
+
   private 
   def item_params
     params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
