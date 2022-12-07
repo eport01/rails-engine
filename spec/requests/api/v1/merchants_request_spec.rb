90b786e5395fb0796fe69b4b21f76d5b13924b32
Emily Port <emilylport@gmail.com>
2022-12-06 18:31:19 -0700

    all tests pass for getting items for a given merchant

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -36,8 +36,28 @@ describe "Merchants API endpoints" do
   end
 
   it "can get all items for a given merchant ID" do 
-    id = create(:merchant).id 
-    get "/api/v1/merchants/#{id}/items"
+    merchant = create(:merchant) 
+    create_list(:item, 3, merchant: merchant)
+    get "/api/v1/merchants/#{merchant.id}/items"
+    merchant_items = JSON.parse(response.body, symbolize_names: true)[:data]
+    expect(merchant_items.count).to eq(3)
+    merchant_items.each do |item|
+      # require 'pry'; binding.pry
+      expect(item).to have_key(:id)
+      expect(item[:id].to_i).to be_an(Integer)
 
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
+
+      expect(item[:attributes]).to have_key(:merchant_id)
+      expect(item[:attributes][:merchant_id]).to be_a(Integer)
+
+    end
   end
 end
